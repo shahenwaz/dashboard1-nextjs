46269ad539ae8fd750d6db0a0c83423d163b7ef7
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-07-09 01:03:06 +0100

    Component added - UserItem

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,13 @@
 "use client";
+
+import UserItem from "./UserItem";
+
 export default function Sidebar() {
   return (
-    <div className="flex flex-col w-[300px] min-w-[300px] border-r min-h-screen p-4">
-      <div>User Part</div>
+    <div className="flex flex-col gap-4 w-[300px] min-w-[300px] border-r min-h-screen p-4">
+      <div>
+        <UserItem />
+      </div>
       <div className="grow">Menu</div>
       <div>Settings / Notifications</div>
     </div>
